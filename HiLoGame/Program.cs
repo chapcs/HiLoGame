0967998f57860b31b0b0d2720b9d6ae8ce51819f
chapcs <benchapman003@gmail.com>
2023-03-12 16:58:48 -0400

    Add project files.

---
+++ HiLoGame/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to HiLo.");
+        Console.WriteLine($"Guess numbers between 1 and {HiLoGame.MAXIMUM}.");
+        HiLoGame.Hint();
+        while (HiLoGame.GetPot() > 0)
+        {
+
+        }
+    }
+}
