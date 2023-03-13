767709db87ed0b5782b039fd7b561a717ad0600e
chapcs <benchapman003@gmail.com>
2023-03-13 18:47:52 -0400

    Main method complete

--- HiLoGame/Program.cs
+++ HiLoGame/Program.cs
@@ -7,7 +7,19 @@
         HiLoGame.Hint();
         while (HiLoGame.GetPot() > 0)
         {
-
+            Console.WriteLine("Press h for higher, 1 for lower, ? to buy a hint,");
+            Console.WriteLine($"or any other key to quit with {HiLoGame.GetPot()}.");
+            char key = Console.ReadKey(true).KeyChar;
+            if (key == 'h') HiLoGame.Guess(true);
+            else if (key == '1') HiLoGame.Guess(false);
+            else if (key == '2') HiLoGame.Hint();
+            else return;
         }
+        Console.WriteLine("The pot is empty and you are poor, bye . . .");
     }
+}
+
+static class HiLoGame
+{
+
 }
